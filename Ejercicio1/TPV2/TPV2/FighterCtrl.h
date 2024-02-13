e72ff8c32e5e5e37941f7e921e943a922af68608
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-02-13 16:21:11 +0100

    Update FighterCtrl.h

--- Ejercicio1/TPV2/TPV2/FighterCtrl.h
+++ Ejercicio1/TPV2/TPV2/FighterCtrl.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <SDL_scancode.h>
+#include "src/game/Container.h"
+#include "src/game/InputComponent.h"
 
-class FighterCtrl
+class FighterCtrl : InputComponent
 {
-	
+public:
+	FighterCtrl();
+
+	inline void setKeys(SDL_Scancode left, SDL_Scancode right, SDL_Scancode stop) {
+		left_ = left;
+		right_ = right;
+		stop_ = stop;
+	}
+
+	inline void setSpeed(float speed) {
+		speed_ = speed;
+	}
+
+	void handleInput(Container *o) override;
+
+private:
+	SDL_Scancode left_;
+	SDL_Scancode right_;
+	SDL_Scancode stop_;
+	float speed_;
 };
