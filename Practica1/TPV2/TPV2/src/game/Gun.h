c127daa01759ff71733fcff90e9be90dbfa9662b
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-02-15 17:03:16 +0100

    jasdfkjsidjaofñlkjldslf

---
+++ Practica1/TPV2/TPV2/src/game/Gun.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <array>
+#include "Component.h"
+#include "../utils/Vector2D.h"
+
+class Gun : public Component
+{
+	//...
+public:
+	struct Bullet
+	{
+		bool used = false;
+		Vector2D pos;
+		Vector2D vel;
+		int width;
+		int height;
+		float rot;
+	};
+
+	constexpr static auto max_bullets = 20;
+	using bullets_array = std::array<Bullet, max_bullets>;
+	using iterator = bullets_array::iterator;
+	void reset();
+
+	iterator begin()
+	{
+		return bullets_.begin();
+	}
+
+	iterator end()
+	{
+		return bullets_.end();
+	}
+	//…
+	
+private:
+	void shoot(Vector2D p, Vector2D v, int width, int height, float r);
+	bullets_array bullets_;
+	//…
+};
