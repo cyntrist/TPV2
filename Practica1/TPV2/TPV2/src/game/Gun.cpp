9e23f6e756c1907be13dde33e854c62ef97e4ff3
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-02-16 14:20:32 +0100

    disapor

--- Practica1/TPV2/TPV2/src/game/Gun.cpp
+++ Practica1/TPV2/TPV2/src/game/Gun.cpp
@@ -6,6 +6,7 @@
 #include "../sdlutils/macros.h"
 #include "../sdlutils/Texture.h"
 #include "../sdlutils/SDLUtils.h"
+#include "../utils/Collisions.h"
 
 constexpr int BULLET_SIZE = 50,
               SHOOT_TIMER = 250;
@@ -35,14 +36,6 @@ void Gun::render()
 
 void Gun::update()
 {
-	for (auto& i : bullets_)
-	{
-		if (i.used)
-		{
-			i.pos = i.pos + i.vel;
-		}
-	}
-
 	if (ih().isKeyDown(SDL_SCANCODE_S))
 	{
 		Vector2D p = transform_->getPos();
@@ -57,6 +50,25 @@ void Gun::update()
 		Vector2D bv = Vector2D(0, -1).rotate(r) * (v.magnitude() + 5.0f);
 		float br = Vector2D(0, -1).angle(bv);
 		shoot(bp, bv, bw, bh, br);
+	}
+
+	for (Bullet& i : bullets_)
+	{
+		if (i.used)
+		{
+			i.pos = i.pos + i.vel;
+			if (!Collisions::collides(
+				Vector2D(0,0),
+				sdlutils().width(),
+				sdlutils().height(),
+				i.pos,
+				i.width,
+				i.height
+				))
+			{
+				i.used = false;
+			}
+		}
 	}
 }
 
@@ -78,20 +90,21 @@ void Gun::shoot(Vector2D p, Vector2D v, int width, int height, float r)
 	lastShot_ = newShot;
 
 	int i = 0;
-	while(bullets_[i].used)
-	{
+	while (i < max_bullets && bullets_[i].used)
 		i++;
+
+	if (i < max_bullets)
+	{
+		Bullet& bullet = bullets_[i];
+		bullet.used = true;
+		bullet.pos = p;
+		bullet.vel = v;
+		bullet.width = width;
+		bullet.height = height;
+		bullet.rot = r;
+
+		sdlutils().soundEffects().at("fire").play();
+
+		std::cout << "lol lmao pew pew " << std::endl;
 	}
-
-	Bullet& bullet = bullets_[i];
-	bullet.used = true;
-	bullet.pos = p;
-	bullet.vel = v;
-	bullet.width = width;
-	bullet.height = height;
-	bullet.rot = r;
-
-	sdlutils().soundEffects().at("fire").play();
-
-	std::cout << "lol lmao pew pew " << std::endl;
 }
