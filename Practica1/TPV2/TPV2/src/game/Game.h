880289b2289e34086869f197a577cc3eeb6b9167
cynthia <118978961+cyntrist@users.noreply.github.com>
2024-02-17 22:02:47 +0100

    states init

--- Practica1/TPV2/TPV2/src/game/Game.h
+++ Practica1/TPV2/TPV2/src/game/Game.h
@@ -1,6 +1,7 @@
 // This file is part of the course TPV2@UCM - Samir Genaim
 
 #pragma once
+#include "GameState.h"
 #include "../ecs/Manager.h"
 #include "../utils/Singleton.h"
 class FighterUtils;
@@ -31,39 +32,39 @@ public:
 
 	void setState(State s)
 	{
-		//GameState* new_state = nullptr;
+		GameState* new_state = nullptr;
 		switch (s)
 		{
 		case RUNNING:
-			//new_state = runing_state_;
+			new_state = runing_state_;
 			break;
 		case PAUSED:
-			//new_state = paused_state_;
+			new_state = paused_state_;
 			break;
 		case NEWGAME:
-			//new_state = newgame_state_;
+			new_state = newgame_state_;
 			break;
 		case NEWROUND:
-			//new_state = newround_state_;
+			new_state = newround_state_;
 			break;
 		case GAMEOVER:
-			//new_state = gameover_state_;
+			new_state = gameover_state_;
 			break;
 		default:
 			break;
 		}
-		/*current_state_->leave();
+		current_state_->leave();
 		current_state_ = new_state;
-		current_state_->enter();*/
+		current_state_->enter();
 	}
 
 private:
-	//GameState *current_state_;
-	//GameState *paused_state_;
-	/*GameState *runing_state_;
-	GameState *newgame_state_;
-	GameState *newround_state_;
-	GameState *gameover_state_;*/
+	GameState* current_state_;
+	GameState* paused_state_;
+	GameState* runing_state_;
+	GameState* newgame_state_;
+	GameState* newround_state_;
+	GameState* gameover_state_;
 	Game();
 	void checkCollisions();
 	FighterUtils* f_utils_;
